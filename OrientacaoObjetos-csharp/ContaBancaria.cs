857f48cd739a3d8a6c4176393e956b380d779418
Rodrigo Seliprin <rseliprin@gmail.com>
2023-04-26 06:27:35 -0300

    Adicionar arquivos de projeto.

---
+++ OrientacaoObjetos-csharp/ContaBancaria.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientacaoObjetos_csharp
+{
+
+    public class ContaBancaria
+    {
+        public string Tipo { get; set; }
+        public int Numero { get; set; }
+        public int Agencia { get; set; }
+        public double Saldo { get; set; }
+
+        public ContaBancaria (string tipo, int numero, int agencia, double saldo)
+        {
+            Tipo = tipo;
+            Numero = numero;
+            Agencia = agencia;
+            Saldo = saldo;
+        }
+
+        public string ExibirDadosConta()
+        {
+            return $"A conta bancária pesquisada é do tipo { Tipo } com a agência { Agencia } e inscrita no número da conta { Numero } com o saldo { Saldo }";
+        }
+    }
+
+}
---
+++ OrientacaoObjetos-csharp/Program.cs
@@ -0,0 +1,57 @@
+﻿using OrientacaoObjetos_csharp;
+
+string mensagem = "";
+
+try
+{
+
+    Console.WriteLine("Bem vindo ao cadastro de contas bancárias");
+
+    List<ContaBancaria> listacontas = new List<ContaBancaria>();
+
+    for (int contagem = 1; contagem <= 3; contagem++)
+    {
+        Console.WriteLine("CADASTRE UMA NOVA CONTA");
+
+        Console.WriteLine($"Digite o tipo {contagem}:");
+        string tipoconta = Console.ReadLine();
+
+        Console.WriteLine($"Digite a agência {contagem}:");
+        int agencia = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Digite o número {contagem}");
+        int numeroconta = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Qual é o saldo {contagem}:");
+        double saldoconta = Convert.ToDouble(Console.ReadLine());
+
+        ContaBancaria contaBancaria = new ContaBancaria(tipoconta, numeroconta, agencia, saldoconta);
+
+        listacontas.Add(contaBancaria);
+    }
+
+    Console.WriteLine("Qual número de conta você gostaria de ver?");
+    int numerocontabusca = Convert.ToInt32(Console.ReadLine());
+
+    if (listacontas.Where(conta => conta.Numero == numerocontabusca).Any())
+    {
+        ContaBancaria contaBancariaBusca = listacontas.Where(conta => conta.Numero == numerocontabusca).FirstOrDefault();
+        mensagem = contaBancariaBusca.ExibirDadosConta();
+    }
+    else
+    {
+        mensagem = "Essa conta não existe";
+    }
+
+}
+catch (Exception ex)     // Para recuperar o erro 
+{
+    mensagem = "O erro encontrado no sistema foi: " + ex.Message;
+}
+finally
+{
+    Console.WriteLine(mensagem);
+}
+
+
+
